e80bfe78ac379957e84988f4e462024a060e2584
FTWRahul <r.poundarik@gmail.com>
2020-07-17 19:20:48 -0400

    Tweening done
    - Imported DoTween
    - Created Sequence
    - Custom expantion Animation Curve

---
+++ Jett_Cloud_URP/Assets/Scripts/Expand.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+
+public class Expand : MonoBehaviour
+{
+    [SerializeField] private float endScale;
+    [SerializeField] private float startScale;
+    [SerializeField] private float endDuration;
+    [SerializeField] private float startDuration;
+    [SerializeField] private float breakDuration;
+    [SerializeField] private Ease endEaseType;
+    [SerializeField] private Ease startEaseType;
+    [SerializeField] private AnimationCurve shrinkEaseCurve;
+    private Sequence mySequence = DOTween.Sequence();
+    
+    public void Grow()
+    {
+        StartCoroutine(GrowSequence());
+    }
+
+    private IEnumerator GrowSequence()
+    {
+        mySequence.Prepend(transform.DOScale(startScale, startDuration).SetEase(shrinkEaseCurve));
+        yield return new WaitForSeconds(startDuration + breakDuration);
+        mySequence.Append(transform.DOScale(endScale, endDuration).SetEase(endEaseType));
+    }
+}
